05fc36b6e3dbc0090c84b9c02daef939526f41d5
bspinner <bspinner@users.noreply.github.com>
2020-03-08 11:29:10 +0100

    Removes deprecated code

--- app/src/main/java/de/codelett/amiberry/ui/startup/StartupActivityFragment.kt
+++ app/src/main/java/de/codelett/amiberry/ui/startup/StartupActivityFragment.kt
@@ -9,7 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import de.codelett.amiberry.AmiberryActivity
 import de.codelett.amiberry.R
 import kotlinx.android.synthetic.main.fragment_startup.*
@@ -24,7 +24,7 @@ class StartupActivityFragment : Fragment() {
         super.onCreate(savedInstanceState)
         this.activity?.let {
             model = it.run {
-                ViewModelProviders.of(it)[StartupViewModel::class.java]
+                ViewModelProvider(it).get(StartupViewModel::class.java)
             }
         }
     }
